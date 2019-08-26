e2f7fd95b9a1912c64542412eae32df7dde50945
Akshatha.Prabhu <akshatha.prabhu@target.com>
2019-08-26 12:50:58 +0530

    added array list

--- src/main/listCollection.java
+++ src/main/listCollection.java
@@ -8,6 +8,8 @@ import java.util.Iterator;
  */
 public class listCollection {
     //List listCollection = new List();
+
+
     public static void main(String[] args) {
 
         ArrayList<String> arrayList = new ArrayList<>();
